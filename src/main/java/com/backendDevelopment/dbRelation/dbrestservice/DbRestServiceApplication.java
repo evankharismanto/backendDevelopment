477d13e1e52c75a98c05076805df659d92cc2faa
evankharismanto <evan.kharismanto@accenture.com>
2020-02-14 20:26:27 +0800

    mission3 - JDBC connection

---
+++ src/main/java/com/backendDevelopment/dbRelation/dbrestservice/DbRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.backendDevelopment.dbRelation.dbrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backendDevelopment/dbRelation/dbrestservice/DbRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendDevelopment.dbRelation.dbrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
