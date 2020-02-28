f17e9293477d30c0bbe91da0575be9c94a7eca0a
evankharismanto <evan.kharismanto@accenture.com>
2020-02-28 12:39:29 +0800

    Mission 6 - Centralizing the properties auto evaluate properties on request

--- helloworld-profiledproperties/src/main/java/com/javadevelopersguide/springboot/example/HelloWorldController.java
+++ helloworld-profiledproperties/src/main/java/com/javadevelopersguide/springboot/example/HelloWorldController.java
@@ -1,50 +1,76 @@
 package com.javadevelopersguide.springboot.example;
-import lombok.Cleanup;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.env.OriginTrackedMapPropertySource;
-import org.springframework.core.env.*;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Properties;
+import java.util.*;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @EnableAutoConfiguration
+@RefreshScope
 public class HelloWorldController {
     @Autowired
     StandardEnvironment environment;
     @Value("${hello.message}")
     private String helloMessage;
+    @Value("${spring.application.name}")
+    private String appName;
+    @Value("${spring.cloud.config.uri}")
+    private String configBaseURI;
+    List<PropertySource<?>> tempPropertySources = new ArrayList<PropertySource<?>>();
 
     @RequestMapping("/hello")
     @ResponseBody
     public String sayHello(@RequestHeader(name = "profile-name", required = false) String profileName) throws IOException {
         if(profileName != null) {
-            reloadPropValue(environment.getActiveProfiles()[0],profileName);
+            var envProfiles = environment.getActiveProfiles();
+            if(environment.getActiveProfiles().length > 1) {
+                if (!envProfiles[1].equals(profileName))
+                    reloadPropValue(envProfiles[1], profileName);
+            }
+            else if(!envProfiles[0].equals(profileName))
+                reloadPropValue(envProfiles[0], profileName);
             environment.setActiveProfiles(profileName.equals("") ? "default": profileName);
             helloMessage = environment.getProperty("hello.message");
         }
         return helloMessage;
     }
 
-    private void reloadPropValue(String currentProfile,String profileName) throws IOException {
+    private void reloadPropValue(String currentProfile,String newProfile) throws IOException {
         MutablePropertySources propertySources = environment.getPropertySources();
         Properties properties = new Properties();
-        profileName = profileName == null || profileName.equals("") ? "" : "-"+profileName;
-        currentProfile = currentProfile == null || currentProfile.equals("default") ? "" : "-"+currentProfile;
-        var configPaths = Paths.get("src/main/resources/application"+profileName+".properties");
-        InputStream inputStream = Files.newInputStream(configPaths);
-                /*getClass().getResourceAsStream("/application"+profileName+".properties");*/
-        properties.load(inputStream);
-        inputStream.close();
-        propertySources.replace("applicationConfig: [classpath:/application"+currentProfile+".properties]",new OriginTrackedMapPropertySource("applicationConfig: [classpath:/application"+profileName+".properties]", properties));
+        newProfile = newProfile == null ? "" : newProfile;
+        currentProfile = currentProfile == null ? "default" : currentProfile;
+        String propertyBaseName = propertySources.stream().filter(r->r.getName().contains(appName+".properties")).findFirst().get().getName();
+        String stringFormatPropertyName = propertyBaseName.replace(".properties","%s.properties");
+        String currentProfileProperty = String.format(stringFormatPropertyName,currentProfile.equals("default") ? "" : "-"+currentProfile);
+        String newProfileProperty = String.format(stringFormatPropertyName,newProfile.equals("") ? newProfile : "-"+newProfile);
+        if(!newProfile.equals("")) {
+            if (!propertySources.stream().anyMatch(r -> r.getName().equals(newProfileProperty)))
+                propertySources.addBefore(propertyBaseName, tempPropertySources.stream().filter(t -> t.getName().equals(newProfileProperty)).findFirst().get());
+        }
+        if(!currentProfile.equals("default")) {
+            if(newProfile.equals("")){
+                List<PropertySource<?>> sources = propertySources.stream().filter(r -> r.getName().contains(propertyBaseName.replace(".properties","-"))).collect(Collectors.toList());
+                for(var propertySourceObj:sources){
+                    if(!tempPropertySources.stream().anyMatch(r -> r.getName().equals(propertySourceObj.getName())))
+                        tempPropertySources.add(propertySourceObj);
+                }
+                for(String resourceName : tempPropertySources.stream().map(r->r.getName()).collect(Collectors.toList())) {
+                    if (propertySources.stream().anyMatch(r -> r.getName().equals(resourceName)))
+                        propertySources.remove(resourceName);
+                }
+            }
+            else if (propertySources.stream().anyMatch(r -> r.getName().equals(currentProfileProperty))) {
+                if(!tempPropertySources.stream().anyMatch(r -> r.getName().equals(currentProfileProperty)))
+                    tempPropertySources.add(propertySources.stream().filter(r -> r.getName().equals(currentProfileProperty)).findFirst().get());
+                propertySources.remove(currentProfileProperty);
+            }
+        }
     }
 }
--- helloworld-profiledproperties/src/main/java/com/javadevelopersguide/springboot/example/HelloWorldSpringBootApplication.java
+++ helloworld-profiledproperties/src/main/java/com/javadevelopersguide/springboot/example/HelloWorldSpringBootApplication.java
@@ -1,18 +1,9 @@
 package com.javadevelopersguide.springboot.example;
 
-import lombok.Cleanup;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.env.*;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Properties;
 
 @SpringBootApplication
 public class HelloWorldSpringBootApplication {
@@ -22,14 +13,4 @@ public class HelloWorldSpringBootApplication {
 	public static void main(String[] args) {
 		context = SpringApplication.run(HelloWorldSpringBootApplication.class, args);
 	}
-
-	/*public static void reloadProfile(String profileName){
-		Thread thread = new Thread(() -> {
-			context.close();
-			context = SpringApplication.run(HelloWorldSpringBootApplication.class, "--spring.profiles.active="+profileName);
-		});
-
-		thread.setDaemon(false);
-		thread.start();
-	}*/
 }
